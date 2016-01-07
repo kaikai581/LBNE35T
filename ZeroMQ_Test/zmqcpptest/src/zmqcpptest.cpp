742e88ca797f175510c1f9755598c56f5a7b259d
kaikai <kaikai5816787@gmail.com>
2016-01-07 15:38:16 -0700

    try out zmq::message_t

--- ZeroMQ_Test/zmqcpptest/src/zmqcpptest.cpp
+++ ZeroMQ_Test/zmqcpptest/src/zmqcpptest.cpp
@@ -7,5 +7,12 @@ int main (int argc, char *argv[])
   zmq::socket_t  sender(context, ZMQ_PUSH);
   sender.connect("tcp://lbne35t-gateway01.fnal.gov:5000");
   
+  char myname[] = "Pierre de Fermat";
+  size_t msgsize = strlen(myname)+1;
+  zmq::message_t message(msgsize);
+  memcpy(message.data(), myname, msgsize);
+  
+  sender.send(message);
+  
   return 0;
 }
